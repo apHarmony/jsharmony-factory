fe063acc04fbd9478fda3a350949c59ad6066da5
Andrew Palczewski <apj@apharmony.com>
2017-10-17 11:25:11 -0500

    Postgres Init Scripts

--- init/pgsql/init.01_ROLES.sql
+++ init/pgsql/init.01_ROLES.sql
@@ -13,9 +13,9 @@ select pg_user.usename,
 from pg_user
 join pg_auth_members on (pg_user.usesysid=pg_auth_members.member)
 join pg_roles on (pg_roles.oid=pg_auth_members.roleid)
-where pg_user.usename = 'jsharmony_%%%INIT_DB_LCASE%%%_user'
+where pg_user.usename = '%%%INIT_DB_USER%%%'
 and   pg_roles.rolname = 'jsharmony_%%%INIT_DB_LCASE%%%_role_exec') then
-  grant jsharmony_%%%INIT_DB_LCASE%%%_role_exec to jsharmony_%%%INIT_DB_LCASE%%%_user;
+  grant jsharmony_%%%INIT_DB_LCASE%%%_role_exec to %%%INIT_DB_USER%%%;
 end if;
 if not exists (
 select pg_user.usename,
@@ -23,9 +23,9 @@ select pg_user.usename,
 from pg_user
 join pg_auth_members on (pg_user.usesysid=pg_auth_members.member)
 join pg_roles on (pg_roles.oid=pg_auth_members.roleid)
-where pg_user.usename = 'jsharmony_%%%INIT_DB_LCASE%%%_user'
+where pg_user.usename = '%%%INIT_DB_USER%%%'
 and   pg_roles.rolname = 'jsharmony_%%%INIT_DB_LCASE%%%_role_dev') then
-  grant jsharmony_%%%INIT_DB_LCASE%%%_role_dev to jsharmony_%%%INIT_DB_LCASE%%%_user;
+  grant jsharmony_%%%INIT_DB_LCASE%%%_role_dev to %%%INIT_DB_USER%%%;
 end if;
 end
 $body$;
