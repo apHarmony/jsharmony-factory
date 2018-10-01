5ef274bbf17f4f15c69d6d0e6a7272b1cb00b69b
Andrew Palczewski <apj@apharmony.com>
2018-10-01 14:54:56 -0500

    X_MODELS loading hang

--- models/X_MODELS.js
+++ models/X_MODELS.js
@@ -4,6 +4,5 @@ jsh.App.X_MODELS.ShowUserModels = function(popupmodelid, parentmodelid, fieldid,
   var searchitems = [];
   searchitems.push(new jsh.XSearch.SearchItem('model_component', 'jsharmonyFactory', 'and', '<>'));
   jsh.App['XForm' + popupmodelid + '_SetFilter'](searchitems, true);
-  jsh.App['XForm' + popupmodelid + '_FilterApply']();
-  onComplete();
+  jsh.App['XForm' + popupmodelid + '_FilterApply'](onComplete);
 }
