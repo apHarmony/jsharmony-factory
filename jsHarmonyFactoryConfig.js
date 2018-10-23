8bba6735ee76a27a1be5b9b71e4d457b753850c2
Andrew Palczewski <apj@apharmony.com>
2018-10-22 23:50:14 -0500

    Move auto-start parameter

--- jsHarmonyFactoryConfig.js
+++ jsHarmonyFactoryConfig.js
@@ -51,7 +51,6 @@ function jsHarmonyFactoryConfig(){
     no_job_email: false,                      //Disable sending EMAILS from JOB
     no_job_sms: false,                        //Disable sending SMS messages from JOB
     disable_job_processor: false,             //Disable Job Processor
-    auto_start_job_processor: true,           //Auto-start Job Processor
   };
 
   //Excel integration token timeout
@@ -60,7 +59,9 @@ function jsHarmonyFactoryConfig(){
   this.JobCheckDelay = 60000;
   //Job Processor sleep time after last job item executed - Default 5000
   this.JobSleepDelay = 5000;
-  
+
+  //Auto-start Job Processor
+  this.auto_start_job_processor = true;
   //Enable scheduled tasks
   this.enable_scheduler = 1;
   //Scheduled task definition
