6bd33c2f168aa29c81b90e5fde4b28d7d2f8c390
Andrew Palczewski <apj@apharmony.com>
2019-06-30 12:34:37 -0500

    JSH-230 Base models in Help Panel

--- models/X_MODELS.js
+++ models/X_MODELS.js
@@ -11,7 +11,7 @@ jsh.App[modelid].ShowUserModels = function(popupmodelid, parentmodelid, fieldid,
 
 jsh.App[modelid].ShowBaseModels = function(popupmodelid, parentmodelid, fieldid, onComplete){
   var searchitems = [];
-  searchitems.push(new jsh.XSearch.SearchItem('module_parent', '', 'and', 'null'));
+  searchitems.push(new jsh.XSearch.SearchItem('model_parents', '', 'and', 'null'));
   var popupmodel = jsh.XModels[popupmodelid];
   popupmodel.controller.SetSearch(searchitems, true);
   popupmodel.controller.RunSearch(onComplete);
