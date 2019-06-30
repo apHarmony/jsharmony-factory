912c3a76979af51b9209847f92929762cc521e70
Andrew Palczewski <apj@apharmony.com>
2019-06-30 11:31:02 -0500

    JSH-230 Config Viewer

--- models/DEV_MODELS.onroute.js
+++ models/DEV_MODELS.onroute.js
@@ -4,9 +4,16 @@ var Helper = require('../Helper.js');
 var ejsext = require('../lib/ejsext.js');
 var model = jsh.getModelClone(req, modelid);
 
-model.oninit = "_this.Models = "+JSON.stringify(_.keys(jsh.Models))+";"+model.oninit;
+model.oninit = "_this.Models = "+JSON.stringify(['System Config'].concat(_.keys(jsh.Models)))+";"+model.oninit;
 if(req.query.modelid){
-  var modeldata = ejsext.escapeJS(JSON.stringify(jsh.getModel(req, req.query.modelid)||{'MODEL':'NOT FOUND'}));
+  var modeldata = {};
+  if(req.query.modelid=='System Config'){
+    modeldata = jsh.Config;
+  }
+  else{
+    modeldata = jsh.getModel(req, req.query.modelid)
+  }
+  modeldata = ejsext.escapeJS(JSON.stringify(modeldata||{'MODEL':'NOT FOUND'}));
   modeldata = modeldata.replace(/script/g, 'scr"+"ipt');
   model.oninit = "_this.ModelData = \""+modeldata+"\";"+model.oninit;
 }
