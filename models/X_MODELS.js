04c923bc08124cc3b2435d6b848f543580b8c93b
Andrew Palczewski <apj@apharmony.com>
2019-06-29 22:13:52 -0500

    JSH-230 Exclude system models

--- models/X_MODELS.js
+++ models/X_MODELS.js
@@ -3,6 +3,7 @@ jsh.App[modelid] = { }
 jsh.App[modelid].ShowUserModels = function(popupmodelid, parentmodelid, fieldid, onComplete){
   var searchitems = [];
   searchitems.push(new jsh.XSearch.SearchItem('model_module', 'jsharmonyFactory', 'and', '<>'));
+  searchitems.push(new jsh.XSearch.SearchItem('model_module', 'jsharmony', 'and', '<>'));
   var popupmodel = jsh.XModels[popupmodelid];
   popupmodel.controller.SetSearch(searchitems, true);
   popupmodel.controller.RunSearch(onComplete);
