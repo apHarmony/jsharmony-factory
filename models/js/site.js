6626b98a405a08c4e3e8494f0a7e5a4ded1866a1
Andrew Palczewski <apj@apharmony.com>
2021-08-11 10:47:49 -0500

    JSH-230 Multiple instance support

---
+++ models/js/site.js
@@ -0,0 +1,19 @@
+(function(jsh){
+  var XExt = jsh.XExt;
+  var XForm = jsh.XForm;
+
+  jsh.System.suggestFeature = function(app_name){
+    XExt.CustomPrompt('.suggest_feature', jsh.RenderEJS(jsh.GetEJS('jsHarmonyFactory.SuggestFeature'),{app_name: app_name}), function () { //onInit
+    }, function (success) { //onAccept
+      //Save content to server
+      var jprompt = jsh.$root('.xdialogblock .suggest_feature');
+      var params = {
+        message_text: jprompt.find('.message_text').val()
+      };
+      XForm.Post('/_funcs/SUGGEST_FEATURE',{},params,function(){
+        success();
+        setTimeout(function(){ XExt.Alert('Thank you for your feedback'); }, 1);
+      });
+    });
+  }
+})(window['{req.jshsite.instance}']);
