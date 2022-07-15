10f05870f87675b1112e8d4ed5159ff48e5acc7a
Andrew Palczewski <apj@apharmony.com>
2022-07-15 17:45:19 -0500

    JSH-230 Support for MSSQL Trusted Auth during Init

--- models/sql/scripts/init/mssql/core.create.02_USER.sql
+++ models/sql/scripts/init/mssql/core.create.02_USER.sql
@@ -1,6 +1,5 @@
-if not exists(select principal_id from sys.server_principals where name = '%%%INIT_DB_USER%%%')
+if ('%%%INIT_DB_USER%%%' <> '') and not exists(select principal_id from sys.server_principals where name = '%%%INIT_DB_USER%%%')
 begin
-  create login %%%INIT_DB_USER%%%
-    with password = '%%%INIT_DB_PASS%%%';
+  exec('create login %%%INIT_DB_USER%%% with password = ''%%%INIT_DB_PASS%%%''');
 end
 GO
--- models/sql/scripts/init/mssql/core.init.02_Copy_Pre.sql
+++ models/sql/scripts/init/mssql/core.init.02_Copy_Pre.sql
@@ -5,12 +5,18 @@ GRANT ALTER ON SCHEMA::[dbo] TO [{schema}_role_dev] AS [dbo];
 GRANT ALTER ON SCHEMA::[{schema}] TO [{schema}_role_dev] AS [dbo];
 GO
 
-if not exists(select principal_id from sys.database_principals where name = '%%%INIT_DB_USER%%%')
+if ('%%%INIT_DB_USER%%%' <> '')
 begin
-  create user %%%INIT_DB_USER%%% for login %%%INIT_DB_USER%%%;
+  if not exists(select principal_id from sys.database_principals where name = '%%%INIT_DB_USER%%%')
+  begin
+    exec('create user %%%INIT_DB_USER%%% for login %%%INIT_DB_USER%%%');
+  end
 end
 GO
 
-alter role {schema}_role_dev add member %%%INIT_DB_USER%%%;
-alter role {schema}_role_exec add member %%%INIT_DB_USER%%%;
+if ('%%%INIT_DB_USER%%%' <> '')
+begin  
+  exec('alter role {schema}_role_dev add member %%%INIT_DB_USER%%%');
+  exec('alter role {schema}_role_exec add member %%%INIT_DB_USER%%%');
+end
 GO
