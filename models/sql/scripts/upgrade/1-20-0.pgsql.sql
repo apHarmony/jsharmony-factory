259466efe2f17f05e9a5b2a94fcb0935ee2b1da8
Andrew Palczewski <apj@apharmony.com>
2024-05-09 16:43:42 -0500

    JSH-230 addBusinessDays function

--- models/sql/scripts/upgrade/1-20-0.pgsql.sql
+++ models/sql/scripts/upgrade/1-20-0.pgsql.sql
@@ -1,5 +1,5 @@
 jsharmony.version_increment('jsHarmonyFactory',1,20,0,0);
 
-alter table {schema}.audit_detail alter column audit_column_name character varying(32) not null;
+alter table {schema}.audit_detail alter column audit_column_name type character varying(32);
 
 %%%RESTART%%%
